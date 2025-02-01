2783893ecf7b7c90cc40f0204a10a8c2eb49e6a5
Chipili Kafwilo <ckafwilo@gmail.com>
2025-02-01 14:17:10 +0200

    docs: configuring swagger and validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurations
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  /**
+   * Swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('Tech assessment APIs')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

3e86546e7ffade30d843d74b1dceab8a905d343b
Chipili Kafwilo <ckafwilo@gmail.com>
2025-02-01 14:38:21 +0200

    docs: configure compodoc for documentation

--- server/src/users/providers/users.service.ts
+++ server/src/users/providers/users.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
 
+/**
+ * Handles connecting to users Table and business logic pertaining to user
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * Inserts a new user into the db
+   */
   public createUser(createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return { id: 0 };
   }
 
+  /**
+   * Returns a user to whom the passed id belongs
+   */
   public findUserByEmail(email: string) {
     return { id: 0, email };
   }
